a6952116c9281391a8f67d8a7011c388f839c03b
mingir2 <40753283+mingir2@users.noreply.github.com>
2020-08-20 23:06:56 -0500

    Remove from_attribute field and implement droplist

--- data/to_graph.py
+++ data/to_graph.py
@@ -4,22 +4,64 @@ import pandas as pd
 import simplejson
 
 
+droplist = [
+ 'FirstBMI',
+ 'MaxBMI',
+ 'STATENUM', # Utah only
+ 'race', # White only
+ 'zip', # Utah only
+ 'AgeFirstBMI',
+ 'AgeMaxBMI',
+ 'Age1D_alcohol',
+ 'Nr.Diag_alcohol',
+ 'Age1D_psychosis',
+ 'Nr.Diag_psychosis',
+ 'Age1D_anxiety-non-trauma',
+ 'Nr.Diag_anxiety-non-trauma',
+ 'Age1D_somatic disorder',
+ 'Nr.Diag_somatic disorder',
+ 'Age1D_eating',
+ 'Nr.Diag_eating',
+ 'Age1D_bipolar spectrum illness',
+ 'Nr.Diag_bipolar spectrum illness',
+ 'Age1D_depression',
+ 'Nr.Diag_depression',
+ 'Age1D_interpersonal trauma',
+ 'Nr.Diag_interpersonal trauma',
+ 'Age1D_PD-Cluster C-anxiety',
+ 'Nr.Diag_PD-Cluster C-anxiety',
+ 'Age1D_PD-Cluster B-emotional',
+ 'Nr.Diag_PD-Cluster B-emotional',
+ 'Age1D_PD',
+ 'Nr.Diag_PD',
+ 'Age1D_Impulse control disorder',
+ 'Nr.Diag_Impulse control disorder',
+ 'Age1D_obesity',
+ 'Nr.Diag_obesity',
+ 'Age1D_cardiovascular',
+ 'Nr.Diag_cardiovascular',
+ 'Age1D_COPD',
+ 'Nr.Diag_COPD',
+ 'Age1D_asthma',
+ 'Nr.Diag_asthma',
+ 'Age1D_immune-autoimmune',
+ 'Nr.Diag_immune-autoimmune',
+]
+
 # Read csv files
 attr = pd.read_csv('TenFamiliesAttributes.csv')
 struct = pd.read_csv('TenFamiliesStructure.csv')
 
-# Indicate attributes
-attr['from_attributes'] = True
 struct = struct.drop_duplicates('personid')
 
 # Merge attributes and structure
 tree = struct.merge(attr, on='personid', how='left')
-tree['from_attributes'] = tree['from_attributes'].fillna(False)
 
 # Clean & touch up
 tree = tree.reset_index(drop=True)
 tree = tree.drop(columns=['RelativeID'])
 tree['id'] = tree['personid']
+tree = tree.drop(columns=droplist)
 
 # Extract nodes from dataframe
 nodes = tree.to_dict('records')
